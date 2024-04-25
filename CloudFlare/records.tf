b5096b3f7a70a3c2ebc6a88f923fcc6d85ff4413
Chay Fadida <chayfadida1997@gmail.com>
2024-04-25 12:16:14 +0000

    test

--- CloudFlare/records.tf
+++ CloudFlare/records.tf
@@ -1,15 +1,36 @@
-resource "cloudflare_record" "www" {
-  zone_id = var.zone_id
-  name    = "terraform321"
-  value   = local.my_ip
-  type    = "A"
+resource "cloudflare_record" "terraform_managed_resource_ebfb358cf173bec6b75698f51d1cdd59" {
+  name    = "terraform1121"
   proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = local.my_ip
+  zone_id = var.zone_id
 }
 
-resource "cloudflare_record" "wwww" {
-  zone_id = var.zone_id
-  name    = "terraform1121"
-  value   = local.my_ip
-  type    = "A"
+resource "cloudflare_record" "terraform_managed_resource_7c374afa71af1320b212d22cb11022d7" {
+  name    = "terraform11"
   proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = local.my_ip
+  zone_id = var.zone_id
 }
+
+resource "cloudflare_record" "terraform_managed_resource_3b911471aa920b776856862e6e3f2a28" {
+  name    = "terraform321"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = local.my_ip
+  zone_id = var.zone_id
+}
+
+resource "cloudflare_record" "terraform_managed_resource_5bccabcb25cb5cec481bac71692606a2" {
+  name    = "terraform"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = local.my_ip
+  zone_id = var.zone_id
+}
+
